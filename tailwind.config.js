efa3434b64c4d7f0d09717608c8f0306421e45fe
amirKhani <ww.amirkhani.i@gmail.com>
2024-09-26 10:25:12 +0330

    initial project

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,9 @@ module.exports = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        vazirmatn: "var(--vazirmatn)",
+      },
     },
   },
   plugins: [],
